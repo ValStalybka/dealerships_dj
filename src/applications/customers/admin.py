af7bdb8ac28e00672f4f4fb6db44222e6dc0346c
ValStalybka <valstalybka@gmail.com>
2023-06-22 12:52:23 +0300

    added model admins

--- src/applications/customers/admin.py
+++ src/applications/customers/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Customers, Profiles
 
-# Register your models here.
+
+@admin.register(Customers)
+class CustomersAdmin(admin.ModelAdmin):
+    fields = ["first_name", "last_name", "email", "balance"]
+    list_display = ["first_name", "last_name", "email", "balance"]
+    ordering = ("last_name", "created_at")
+    search_fields = ("first_name", "last_name")
+
+
+@admin.register(Profiles)
+class ProfilesAdmin(admin.ModelAdmin):
+    fields = ["customer_id", "bio", "birthday"]
+    list_display = ["customer_id", "bio", "birthday", "created_at"]
+    ordering = ("customer_id",)
+    search_fields = ("customer_id", )

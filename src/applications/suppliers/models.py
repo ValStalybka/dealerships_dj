4a85d6c374b9ac2f9a1e956e02774d19006df7f9
ValStalybka <valstalybka@gmail.com>
2023-06-27 13:06:08 +0300

    changed models

--- src/applications/suppliers/models.py
+++ src/applications/suppliers/models.py
@@ -11,11 +11,13 @@ class Suppliers(CommonInfo):
         validators=[MinValueValidator(1900), MaxValueValidator(2023)]
     )
     cars = models.ManyToManyField(
-        "dealerships.Cars", through="SupplierCars", related_name="suppliers"
+        "dealerships.Cars", through="SupplierCars", related_name="suppliers", blank=True,
     )
 
     class Meta:
         ordering = ["name", "year_founded"]
+        verbose_name = "Supplier"
+        verbose_name_plural = "Suppliers"
 
     def __str__(self):
         return self.name
@@ -28,11 +30,20 @@ class SupplierCars(CommonInfo):
     car = models.ForeignKey("dealerships.Cars", on_delete=models.CASCADE)
     price = MoneyField(max_digits=10, decimal_places=2, default_currency="USD")
     dealership = models.ForeignKey(
-        "dealerships.Dealerships", on_delete=models.CASCADE, related_name="bought"
+        "dealerships.Dealerships", on_delete=models.CASCADE, default=None,
+        blank=True, null=True, related_name="bought"
     )
+
+    class Meta:
+        verbose_name = "SupplierCar"
+        verbose_name_plural = "SupplierCars"
 
 
 class SupplierDiscounts(AbstractDiscount):
     supplier = models.ForeignKey(
         Suppliers, on_delete=models.CASCADE, related_name="discounts"
     )
+
+    class Meta:
+        verbose_name = "SupplierDiscount"
+        verbose_name_plural = "SupplierDiscounts"
